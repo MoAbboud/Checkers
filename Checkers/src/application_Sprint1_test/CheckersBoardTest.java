84530e03c2c332aa2d71c6e3656db931f38fd8b7
MoAbboud <89588118+MoAbboud@users.noreply.github.com>
2021-10-11 16:18:27 -0500

    Added Test

---
+++ Checkers/src/application_Sprint1_test/CheckersBoardTest.java
@@ -0,0 +1,14 @@
+package application_Sprint1_test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CheckersBoardTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
