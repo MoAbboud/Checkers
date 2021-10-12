24ffa993e3358550ae5d23ad51a3e9b4f4bec5a9
MoAbboud <89588118+MoAbboud@users.noreply.github.com>
2021-10-12 17:45:17 -0500

    New Updated for Sprint1

--- Checkers/src/application_Sprint1_prod/Board.java
+++ Checkers/src/application_Sprint1_prod/Board.java
@@ -1,27 +1,53 @@
-package application_Sprint1_prod;
+package sprint1_prod;
 
-import javafx.scene.layout.Pane;
-import javafx.scene.shape.Rectangle;
+public class Board {
 
-public class Board extends Pane {
-
-	private int[][] board;
+	private static int[][] board;
 	private String turn = "Black";
 
 	public Board() {
 		board = new int[8][8];
+
+		for (int i = 0; i < 8; i++) {
+			for (int j = 0; j < 8; j++) {
+				if (i == 0) {
+					if (j % 2 == 1) {
+						board[i][j] = 1;
+					}
+				} else if (i == 1) {
+					if (j % 2 == 0) {
+						board[i][j] = 1;
+					}
+				} else if (i == 2) {
+					if (j % 2 == 1) {
+						board[i][j] = 1;
+					}
+				} else if (i == 5) {
+					if (j % 2 == 0) {
+						board[i][j] = 1;
+					}
+				} else if (i == 6) {
+					if (j % 2 == 1) {
+						board[i][j] = 1;
+					}
+				} else if (i == 7) {
+					if (j % 2 == 0) {
+						board[i][j] = 1;
+					}
+				}
+			}
+		}
 	}
-	
-	public int getCell(int row, int column) {		
+
+	public int getCell(int row, int column) {
 		if (row >= 0 && row < 8 && column >= 0 && column < 8)
 			return board[row][column];
-		else 
+		else
 			return -1;
-
 	}
 
 	public String getTurn() {
 		return turn;
 	}
-	
+
 }
--- Checkers/src/application_Sprint1_test/CheckersBoardTest.java
+++ Checkers/src/application_Sprint1_test/CheckersBoardTest.java
@@ -1,38 +1,87 @@
-package application_Sprint1_test;
+package sprint1_test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.jupiter.api.Test;
 
 import application_Sprint1_prod.Board;
-import application_Sprint1_prod.CheckersGUI;
-import javafx.stage.Stage;
 
 class CheckersBoardTest {
 
-	Stage stage = new Stage();
-	Board board;
-	
+	private Board board = new Board();
+	int count = 0;
+
 	@Before
 	public void setUp() throws Exception {
 
-		board = new Board();
-	}
-	
-	@After
-	public void tearDown() throws Exception {
-		
 	}
 
-	@Test
-	public void testEmptyBoardWithPieces() {
-		
-		try {
-			Thread.sleep(2000);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
+	@After
+	public void tearDown() throws Exception {
+
 	}
+
+	// acceptance criterion 3.1
+	@Test
+	public void testBoardContainsPieces() {
+		for (int row = 0; row < 8; row++) {
+			for (int column = 0; column < 8; column++) {
+				if (row == 0 || row == 2) {
+					if (column % 2 == 0) {
+						assertEquals("", board.getCell(row, column), 0);
+					} else {
+						assertEquals("", board.getCell(row, column), 1);
+					}
+				} else if (row == 1) {
+					if (column % 2 == 0) {
+						assertEquals("", board.getCell(row, column), 1);
+					} else {
+						assertEquals("", board.getCell(row, column), 0);
+					}
+				} else if (row == 5 || row == 7) {
+					if (column % 2 == 0) {
+						assertEquals("", board.getCell(row, column), 1);
+					} else {
+						assertEquals("", board.getCell(row, column), 0);
+					}
+				} else if (row == 6) {
+					if (column % 2 == 0) {
+						assertEquals("", board.getCell(row, column), 0);
+					} else {
+						assertEquals("", board.getCell(row, column), 1);
+					}
+				}
+			}
+		}
+		assertEquals("", board.getTurn(), "Black");
+	}
+
+	// acceptance criterion 3.2
+	@Test
+	public void testValidRow() {
+		assertEquals("", board.getCell(7, 0), 1);
+		assertEquals("", board.getCell(7, 1), 0);
+	}
+
+	// acceptance criterion 3.3
+	@Test
+	public void testValidColumn() {
+		assertEquals("", board.getCell(0, 7), 1);
+		assertEquals("", board.getCell(1, 7), 0);
+	}
+
+	// acceptance criterion 3.4
+	@Test
+	public void testInvalidRow() {
+		assertEquals("", board.getCell(8, 0), -1);
+	}
+
+	// acceptance criterion 3.5
+	@Test
+	public void testInvalidColumn() {
+		assertEquals("", board.getCell(0, 8), -1);
+	}
+
 }
