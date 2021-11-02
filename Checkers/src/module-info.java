b084d237265f674c55c9866b22b6ed6ef467c36d
MoAbboud <89588118+MoAbboud@users.noreply.github.com>
2021-11-02 10:39:07 -0500

    Added new features

--- Checkers/src/module-info.java
+++
@@ -1,9 +0,0 @@
-module Checkers {
-	requires javafx.controls;
-	requires javafx.graphics;
-	requires javafx.fxml;
-	requires junit;
-	requires org.junit.jupiter.api;
-	
-	opens application_Sprint1_prod to javafx.graphics, javafx.fxml;
-}
